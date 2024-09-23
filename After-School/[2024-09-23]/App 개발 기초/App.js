516ad395ccd370250789b92c5a50dc21db7dd96b
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-23 18:49:09 +0900

    [2024-09-23]

--- After-School/[2024-09-23]/App 개발 기초/App.js
+++ After-School/[2024-09-23]/App 개발 기초/App.js
@@ -2,9 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Expo React Native')
+
+  const name = 'Seongjun';
+  const isFullname = true;
+
+  const add = (a, b) => {
+    return a + b;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>a + 2 = {add(1, 2)}</Text>
+      <Text>{isFullname ? name + ' Jang' : name}</Text>
       <StatusBar style="auto" />
     </View>
   );
