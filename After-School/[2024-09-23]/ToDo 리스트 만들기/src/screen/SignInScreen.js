313ce77fc23cdff18394552ae013b87d5b97cae2
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-23 18:53:52 +0900

    [2024-09-23]

---
+++ After-School/[2024-09-23]/ToDo 리스트 만들기/src/screen/SignInScreen.js
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const SignInScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../assets/test.png')} style={styles.Image} />
+      <Text>SignIn Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default SignInScreen;
