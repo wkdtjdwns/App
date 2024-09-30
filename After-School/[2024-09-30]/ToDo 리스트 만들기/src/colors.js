525649a29cf03bee09b7660c9d279095ed0a18a7
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-30 18:31:18 +0900

    [2024-09-30]

---
+++ After-School/[2024-09-30]/ToDo 리스트 만들기/src/colors.js
@@ -0,0 +1,12 @@
+export const WHITE = '#ffffff';
+export const BLACK = '#000000';
+
+export const PRIMARY = {
+  LIGHT: '#93c5fd',
+  DEFAULT: '#2563eb',
+  DARK: '#1e3a8a',
+};
+
+export const GRAY = {
+  DEFAULT: '#a3a3a3',
+};
